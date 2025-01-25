240f085db51acd7726b805402f66a9b1e4504d79
ducky <gillianthorne1353@gmail.com>
2025-01-24 20:45:39 -0500

    fine-tuning the look of the heatmap

--- shots-heatmap/shots-heatmap.py
+++ shots-heatmap/shots-heatmap.py
@@ -6,11 +6,12 @@ import matplotlib.image as mpimg
 # basically just to have all shots mirrored on one side
 # this isn't perfect bc some shots might be taken from across center ice, especially empty net goals
 # but also the threshold will probably weed them out lmao
+# i'm also stretching it to fit the image - it might not be real world accurate but it is good for display purposes
 def do_math(x):
     if x <= 300:
-        return x
+        return round(x/300 * 322)
     else: 
-        return x - 300
+        return round((x - 300)/300 * 322)
 
 # get the data into a dataframe
 # it is already nice and prepared for me!
@@ -25,18 +26,28 @@ pd_allshots["x_location"] = pd_allshots["x_location"].map(lambda x: do_math(x))
 pd_goals["x_location"] = pd_goals["x_location"].map(lambda x: do_math(x))
 pd_nongoals["x_location"] = pd_nongoals["x_location"].map(lambda x: do_math(x))
 
+print(pd_allshots["x_location"].max(axis=0))
+
 # this just gives me the background picture
-map_image = mpimg.imread("extra-resources/images/ht-ice-rink.png")
+map_image = mpimg.imread("extra-resources/images/ht-ice-rink-2.png")
 
 # set up the graph and make three subplots
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(9.5, 3))
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4))
+
+fig.suptitle("2024 Regular Season Shot Map", fontsize="x-large")
+
+# go through each axes and set the size and the x/y ticks (remove them and the labels)
+for i in range(3):
+    axes[i].set_xticks([],[])
+    axes[i].set_yticks([],[])
 
 # graph it!
 # ax is the plot, data/x/y are self explanatory, i want it filled in (heatmap look), coloured rainbow, 80% opacity with a threshold of 30%
 # thresh explained here: https://seaborn.pydata.org/generated/seaborn.kdeplot.html
 graph_allshots = sns.kdeplot(ax=axes[0], data=pd_allshots, x="x_location", y="y_location", fill=True, cmap="rainbow", alpha=0.8)
+# show image with the image, aspect ratio is auto and order is -1 to send to back
 graph_allshots.imshow(map_image,
-                        aspect="auto",
+                        aspect="equal",
                         zorder=-1
                     )
 # give me a title :)
@@ -45,7 +56,7 @@ axes[0].set_title(f"All Shots: {len(pd_allshots)} data points")
 # another graph
 graph_goals = sns.kdeplot(ax=axes[1], data=pd_goals, x="x_location", y="y_location", fill=True, cmap="rainbow", alpha=0.8, thresh=0.2)
 graph_goals.imshow(map_image,
-                        aspect="auto",
+                        aspect="equal",
                         zorder=-1
                     )
 axes[1].set_title(f"All Goals: {len(pd_goals)} data points")
@@ -53,7 +64,7 @@ axes[1].set_title(f"All Goals: {len(pd_goals)} data points")
 # and one last graph!
 graph_nongoals = sns.kdeplot(ax=axes[2], data=pd_nongoals, x="x_location", y="y_location", fill=True, cmap="rainbow", alpha=0.8)
 graph_nongoals.imshow(map_image,
-                        aspect="auto",
+                        aspect="equal",
                         zorder=-1
                     )
 axes[2].set_title(f"All Non-Goals: {len(pd_nongoals)} data points")
